a0c8cce14f8163383c85f7c59b724799bddf6022
Yoshihiko Kuga <horrormovies.noga@gmail.com>
2023-12-19 23:56:23 +0900

    #17 feat: actions 追加

---
+++ vscode-extensions-recommendations/src/app/actions.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+import { exec } from "child_process";
+
+interface FrontMatter {
+  [prop: string]: string;
+}
+
+interface MarkdownDocument {
+  frontMatter: FrontMatter;
+  /** markdown の記述 */
+  content: string;
+}
+
+const ROOT_PATH = join(process.cwd(), "..");
+
+const RECOMMENDATIONS_PATH = join(
+  ROOT_PATH,
+  ".vscode/extensions/recommendations"
+);
+
+/**
+ * `recommendations/` 配下の `.md` による拡張の記述を取得する
+ * @url https://github.com/juristr/blog-series-nextjs-nx/blob/main/libs/markdown/src/lib/markdown.ts
+ */
+export const getParsedFileContents = async (): Promise<MarkdownDocument[]> => {
+  return new Promise((resolve) => {
+    exec(`find ${RECOMMENDATIONS_PATH} -type f`, (_error, stdout) => {
+      console.time();
+
+      const documents = stdout
+        .split(/\r|\n/g)
+        .filter((v) => !!v)
+        .map((fullPath) => {
+          const path = fullPath.replace(ROOT_PATH, "..");
+          const { data, content } = matter(fs.readFileSync(path));
+          return {
+            frontMatter: data,
+            content,
+          };
+        });
+      resolve(documents);
+
+      console.timeEnd();
+    });
+  });
+};
